6f2fc82eff2b5efcae24437a68b5e19f489d08a0
JC <yjc006873@gmail.com>
2026-08-25 10:44:25 +1000

    Initial Commit

---
+++ BlocLens/BlocLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocLensApp.swift
+//  BlocLens
+//
+//  Created by Jiacheng Yu on 25/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct BlocLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
